957e64a478b1f52fddbe8dc009813796827910f6
imamuratakuto <love.east32@gmail.com>
2022-03-23 15:42:22 +0000

    総合課題

--- html/index.php
+++ html/index.php
@@ -32,10 +32,9 @@ function logout()
 if ($_POST) { /* POST Requests */
     if (isset($_POST['logout'])) { //ログアウト処理
         logout();
-        header("Location: login.php");
     } else if (isset($_POST['tweet_textarea'])) { //投稿処理
         if (isset($_POST['reply_post_id'])) {
-            newReplyTweet($_POST['tweet_textarea'],$_POST['reply_post_id']);
+            newReplyTweet($_POST['tweet_textarea'], $_POST['reply_post_id']);
         } else {
             newtweet($_POST['tweet_textarea']);
         }
